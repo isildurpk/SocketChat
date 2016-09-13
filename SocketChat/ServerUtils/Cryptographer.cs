d33bfbd559af79f0a69d6f76e47970c3fbe6b8cd
isildurpk <isildurpk@gmail.com>
2016-09-13 22:26:51 +0700

    Encrypting/decrypting

--- SocketChat/ServerUtils/Cryptographer.cs
+++ SocketChat/ServerUtils/Cryptographer.cs
@@ -1,22 +1,45 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using SecurityDriven.Inferno;
 
 namespace ServerUtils
 {
     public static class Cryptographer
     {
+        private static readonly CryptoRandom Random = new CryptoRandom();
+
         public static byte[] Encrypt(byte[] data, byte[] keyBlob)
         {
-            return data;
+            using (var input = new MemoryStream(data))
+            using (var output = new MemoryStream())
+            using (var transform = new EtM_EncryptTransform(keyBlob))
+            {
+                using (var cryptoStream = new CryptoStream(output, transform, CryptoStreamMode.Write))
+                    input.CopyTo(cryptoStream);
+                return output.ToArray();
+            }
         }
 
         public static byte[] Decrypt(byte[] data, byte[] keyBlob)
         {
-            return data;
+            using (var input = new MemoryStream(data))
+            using (var output = new MemoryStream())
+            using (var transform = new EtM_DecryptTransform(keyBlob))
+            {
+                using (var cryptoStream = new CryptoStream(input, transform, CryptoStreamMode.Read))
+                    cryptoStream.CopyTo(output);
+
+                if (!transform.IsComplete)
+                    throw new Exception("Не все блоки расшифрованы!");
+
+                return output.ToArray();
+            }
         }
 
         public static byte[] GenerateKey()
         {
-            return Guid.NewGuid().ToByteArray();
+            return Random.NextBytes(32);
         }
     }
 }
